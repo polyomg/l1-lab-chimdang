08e25b0a212cad039a0fcf4c9b9d4f5f445de826
HoangAnh <hoanganh070607@gmail.com>
2025-10-16 07:52:14 +0700

    TS01073 - DangTranHoangAnh - LAB6

---
+++ Lab5/src/main/java/poly/demo/Ts01073Lab5Application.java
@@ -0,0 +1,13 @@
+package poly.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ts01073Lab5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ts01073Lab5Application.class, args);
+	}
+
+}
---
+++ Lab5/src/main/java/poly/demo/service/ShoppingCartService.java
@@ -0,0 +1,17 @@
+package poly.demo.service;
+
+
+
+import poly.demo.entity.Item;
+
+import java.util.Collection;
+
+public interface ShoppingCartService {
+    Item add(Integer id);
+    void remove(Integer id);
+    Item update(Integer id, int qty);
+    void clear();
+    Collection<Item> getItems();
+    int getCount();
+    double getAmount();
+}
