7071558914ec33039fd4306b52cb685b620023bb
HoangAnh <hoanganh070607@gmail.com>
2025-10-16 07:53:23 +0700

    TS01073 - DangTranHoangAnh -LAB6

---
+++ Lab6/src/main/java/poly/edu/dao/AccountDAO.java
@@ -0,0 +1,8 @@
+package poly.edu.dao; 
+
+import poly.edu.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountDAO extends JpaRepository<Account, String> {
+	
+}
---
+++ Lab6/src/main/java/poly/edu/dao/OrderDAO.java
@@ -0,0 +1,6 @@
+package poly.edu.dao; 
+import poly.edu.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderDAO extends JpaRepository<Order, Long> {
+}
